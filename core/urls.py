7c85b472bdb7d8a045be35c65bee367df54b2768
ismayilovasgar <ismayilovv.asgarr.21@hotmail.com>
2025-03-25 20:33:07 +0400

    save

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from task.views import *
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    ##
+    ## <== API ==>
     path("api/v1/account/", include("account.api.urls")),
     path("api/v1/tasks/", include("task.api.urls")),
+    ##
+    ## <== Template ==>
+    path("dashboard/", dashboard, name="dashboard"),
 ]
