c70f15c2b520a872028c4f93bb65de2d7599edfb
ismayilovasgar <ismayilovv.asgarr.21@hotmail.com>
2025-03-27 02:27:00 +0400

    save

---
+++ README.md
@@ -0,0 +1,5 @@
+# Layihə Adı
+
+Bu layihə [layihənin qısa təsviri]. Django və Django REST Framework (DRF) istifadə edilərək hazırlanmışdır.
+
+## Tələblər (Requirements)
--- core/settings.py
+++ core/settings.py
@@ -87,7 +87,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
-LANGUAGE_CODE = "en-us"
+# LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "az"
 TIME_ZONE = "Asia/Baku"
 USE_I18N = True
 USE_TZ = True
